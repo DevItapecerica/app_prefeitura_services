9d6c74d25250faa7d878db40bf40ac0f7e5b769d
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-03-21 16:45:31 -0300

    `Removed database models and configuration files`

--- server.js
+++ server.js
@@ -3,42 +3,14 @@ const fastifyCors = require("@fastify/cors");
 const fastifySwagger = require("@fastify/swagger");
 const fastifySwaggerUi = require("@fastify/swagger-ui");
 const router = require("./routes/router");
+const swaggerConfig = require('./config/swaggerConfig');
 
 const port = 8003;
 const app = fastify();
 
 app.register(fastifyCors);
 
-app.register(fastifySwagger, {
-  openapi: {
-    openapi: "3.0.0",
-    components: {
-      securitySchemes: {
-        APIKey: {
-          type: "apiKey",
-          in: "header",
-          name: "x-api-key",
-          description: "Use a chave de API no cabeçalho como 'x-api-key'",
-        },
-      },
-    },
-    info: {
-      title: "Test swagger",
-      description: "API principal de consumo de microserviços",
-      version: "2.0.0",
-    },
-    servers: [
-      {
-        url: `http://192.168.16.13:${port}`,
-        description: "Development server",
-      },
-      {
-        url: `http://192.168.16.80:${port}`,
-        description: "prodution server",
-      },
-    ],
-  },
-});
+app.register(fastifySwagger, swaggerConfig);
 
 app.register(fastifySwaggerUi, {
   routePrefix: "/docs",
